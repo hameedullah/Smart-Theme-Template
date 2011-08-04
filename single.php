e968d7026af5a3d33af74b9a2841bac9368bf412
Hameedullah Khan <h@hameedullah.com>
2011-08-04 12:51:18 +0500

    Add single post template

---
+++ single.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The post template
+ */
+
+get_header(); ?>
+
+	<div id="main" role="main">
+		
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<nav id="nav-single">
+				<h3 class="assistive-text"><?php _e( 'Post navigation', 'stt' ); ?></h3>
+				<span class="nav-previous"><?php previous_post_link( '%link', __( 'Previous', 'stt' ) ); ?></span>
+				<span class="nav-next"><?php next_post_link( '%link', __( 'Next', 'stt' ) ); ?></span>
+			</nav>
+
+			<?php get_template_part( 'content', 'single' ); ?>
+
+			<?php comments_template( '', true ); ?>
+
+		<?php endwhile; // End of while have_posts ?>
+
+	</div><!-- #main -->
+
+<?php get_footer(); ?>
