2157fa7db0877d0430d2648b3a93764ae8c4c4de
Hameedullah Khan <h@hameedullah.com>
2011-08-04 12:32:04 +0500

    Add basic sidebar template

---
+++ sidebar.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Right Sidebar i.e Main Sidebar Template 
+ */
+?>
+
+	<div id="right-sidebar" class="widget-area" rol="complementary">
+		<?php if ( ! dynamic_sidebar( 'right-sidebar' ) ) : ?>
+
+			<aside id="archives" class="widget">
+				<h3 class="widget-title"><?php _e( 'Archives', 'stt' ); ?></h3>
+				<ul>
+					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+				</ul>
+			</aside>
+
+			<aside id="meta" class="widget">
+				<h3 class="widget-title"><?php _e( 'Meta', 'stt' ); ?></h3>
+				<ul>
+					<?php wp_register(); ?>
+					<li><?php wp_loginout(); ?></li>
+					<?php wp_meta(); ?>
+				</ul>
+			</aside>
+		<?php endif; // End if dynamic_sidebar right-sidebar?>
+	</div><!-- #right-sidebar .widget-area -->
