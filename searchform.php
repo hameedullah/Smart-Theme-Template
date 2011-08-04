e63886a7a8fabd14bbde02571e806d43d1d2c57c
Hameedullah Khan <h@hameedullah.com>
2011-08-04 14:30:22 +0500

    Add the default search form

---
+++ searchform.php
@@ -0,0 +1,8 @@
+<?php
+
+?>
+	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+		<label for="s" class="assistive-text"><?php _e( 'Search', 'stt' ); ?></label>	
+		<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'stt' ); ?>" />
+		<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'stt' ); ?>" />
+	</form>
