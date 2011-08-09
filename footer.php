01a7e7ec6f53e211653f86f75fb82e3304431217
Hameedullah Khan <h@hameedullah.com>
2011-08-09 14:34:52 +0500

    Add wp_footer call to the footer.php

--- footer.php
+++ footer.php
@@ -32,6 +32,13 @@
     <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
     <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
   <![endif]-->
+
+	<?php
+		/*
+		 * Call to wp_footer
+		 */
+			wp_footer();
+	?>
   
 </body>
 </html>
